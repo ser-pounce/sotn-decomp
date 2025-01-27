d023a34380a5ee0a5198726f70f08ad72d28852c
luksy <ser-pounce@users.noreply.github.com>
2025-01-27 22:09:24 +0100

    Map sprintf data

--- src/main/psxsdk/libc/sprintf.c
+++ src/main/psxsdk/libc/sprintf.c
@@ -3,4 +3,5 @@
 const char a0123456789abcd_0[] = "0123456789ABCDEF";
 const char a0123456789abcd_1[] = "0123456789abcdef";
 
+int D_8002D3A8[3] = {0};
 INCLUDE_ASM("main/nonmatchings/psxsdk/libc/sprintf", sprintf);
